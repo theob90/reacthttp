113a47b611c58fb5f8e60f4529906b3c296c8f55
theob90 <duski.bez@gmail.com>
2020-06-15 19:39:20 +0300

    router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,7 +5,7 @@ import './Blog.css';
 //eiani t instance t katw
 import axios from '../../axios';
 import Posts from '../Posts/Posts'
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
@@ -19,8 +19,11 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname:'/new-post',
+                                
+                            }}>New post</Link></li>
                         </ul>
                     </nav>
                 </header>
